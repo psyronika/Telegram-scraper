f56aeb080a914531a2b50f5498d6bb9204cbb5de
psyronika <m.simon@uva.nl>
2021-06-07 14:22:21 +0200

    reset event loop

--- Telegram_groups.py
+++ Telegram_groups.py
@@ -2,12 +2,13 @@
 
 from telethon import TelegramClient
 import pandas as pd
+import asyncio
 
 
 chat = 'wilderspvv'
 api_id = 5521822
 api_hash = '44208a895cc9a3ed25eb161e9eaad9f7'
-client = TelegramClient('test', api_id, api_hash)
+#client = TelegramClient('test', api_id, api_hash)
 
 '''
 #source = pd.read_csv('/Users/m.simonuva.nl/Documents/GitHub/Telegram-scraper/Telegram-scraper/Telegram_chats.csv')
@@ -18,11 +19,33 @@ async def get_chat(chats):
 		print(chat)
 '''
 # Note `async with` and `async for`
+
+#try to get event loop
+def get_or_create_eventloop():
+    try:
+        return asyncio.get_event_loop()
+    except RuntimeError as ex:
+        if "There is no current event loop in thread" in str(ex):
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            return asyncio.get_event_loop()
+
+# if not event loop create new
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+
+#conect to client
+client = TelegramClient('session_name', api_id, api_hash)
+client.start()
+
+'''
 async def main():
 	try:
 		await client.connect()
 	except OSError:
 		print('Failed to connect')
+'''
+# get message date, sender id, message id, message text
 
 async with client:
 	result = []
@@ -30,5 +53,5 @@ async with client:
 		result.append([msg.date, msg.sender_id, msg.id, msg.text])
 
 df = pd.DataFrame(result, columns = ['Date', 'Sender_ID', 'Message_ID', 'Text'])
-df.to_csv(r'/Users/m.simonuva.nl/Documents/GitHub/Telegram-scraper/pvv.csv', index = False)
+df.to_csv(r'/Users/m.simonuva.nl/Documents/GitHub/Telegram-scraper/geert.csv', index = False)
 
